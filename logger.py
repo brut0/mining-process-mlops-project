15c8be230b162392b91a1ed4b1ca6fa84c406c0c
sergey.zemskov <sergzemsk@yandex.ru>
2022-09-03 21:22:01 +0300

    Logger: possibility to set full log filename

--- logger.py
+++ logger.py
@@ -1,23 +1,36 @@
-# Logger class based on 'loguru'
-# Developer: Sergey Zemskov
+"""
+Logger class based on 'loguru'
+
+Works as singleton
+"""
+
+# Authors: Sergey Zemskov
 
 import sys
+
 from loguru import logger
 
 
 class Singleton(type):
     _instances = {}
+
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
+
 class Logger(metaclass=Singleton):
     created = None
 
-    def __init__(self, filename='ml'):
+    def __init__(self, filename='log.log'):
         if not self.created:
             logger.remove()
             logger.add(sys.stderr, format="{time} | {level} | {message}")
-            logger.add(f"{filename}.log", format="{time} | {level} | {message}", level='TRACE', rotation="100 MB")
+            logger.add(
+                f"{filename}",
+                format="{time} | {level} | {message}",
+                level='TRACE',
+                rotation="100 MB",
+            )
             self.created = True
